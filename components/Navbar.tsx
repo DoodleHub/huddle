4919ea8f47cb8c00845e896e71415bd9f8aa982b
Chao Chen <chaochen@live.com>
2024-04-07 19:32:48 -0400

    feat: create mobile nav component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
@@ -17,6 +18,9 @@ const Navbar = () => {
           Huddle
         </p>
       </Link>
+      <div className="flex-between gap-5">
+        <MobileNav />
+      </div>
     </nav>
   );
 };
