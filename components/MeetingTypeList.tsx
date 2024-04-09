c9697410474a1a5f4a5594dcd0668ce6a604bd8e
Chao Chen <chaochen@live.com>
2024-04-08 20:18:14 -0400

    feat: create reusable home card and other meeting type cards

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,28 +1,46 @@
 'use client';
 
-import Image from 'next/image';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import HomeCard from './HomeCard';
 
 const MeetingTypeList = () => {
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
+  >();
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      <div
-        className="bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer"
-        onClick={() => {}}
-      >
-        <div className="flex-center glassmorphism size-12 rounded-[10px]">
-          <Image
-            src="/icons/add-meeting.svg"
-            alt="meeting"
-            width={27}
-            height={27}
-          />
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <h1 className="text-2xl font-bold">New Meeting</h1>
-          <p className="text-lg font-normal">Start an instant meeting</p>
-        </div>
-      </div>
+      <HomeCard
+        className="bg-orange-1"
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState('isInstantMeeting')}
+      />
+      <HomeCard
+        className="bg-blue-1"
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        handleClick={() => setMeetingState('isScheduleMeeting')}
+      />
+      <HomeCard
+        className="bg-purple-1"
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Check out your recordings"
+        handleClick={() => router.push('/recordings')}
+      />
+      <HomeCard
+        className="bg-yellow-1"
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitation link"
+        handleClick={() => setMeetingState('isJoiningMeeting')}
+      />
     </section>
   );
 };
