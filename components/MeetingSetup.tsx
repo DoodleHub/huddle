97196ba620e44d182299d1c2d369854bcf120ca2
Chao Chen <chaochen@live.com>
2024-04-09 22:54:59 -0400

    feat: add join configs

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,7 +1,16 @@
-import { VideoPreview, useCall } from '@stream-io/video-react-sdk';
-import { useEffect, useState } from 'react';
+import {
+  DeviceSettings,
+  VideoPreview,
+  useCall,
+} from '@stream-io/video-react-sdk';
+import { type Dispatch, type SetStateAction, useEffect, useState } from 'react';
+import { Button } from './ui/button';
 
-const MeetingSetup = () => {
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: Dispatch<SetStateAction<boolean>>;
+}) => {
   const [isMicCamToggleOn, setIsMicCamToggleOn] = useState(false);
   const call = useCall();
 
@@ -23,6 +32,27 @@ const MeetingSetup = () => {
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-2xl font-bold">Setup</h1>
       <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggleOn}
+            onChange={(e) => setIsMicCamToggleOn(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-green-500 px-4 py-2.5"
+        onClick={() => {
+          call.join();
+
+          setIsSetupComplete(true);
+        }}
+      >
+        Join Meeting
+      </Button>
     </div>
   );
 };
