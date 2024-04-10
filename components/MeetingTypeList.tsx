089b6d08f7ab110db4af8f5fd4fb493d4dcfc567
Chao Chen <chaochen@live.com>
2024-04-09 22:22:17 -0400

    feat: implement shadcn toast

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,11 +2,12 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useUser } from '@clerk/nextjs';
 
 import HomeCard from './HomeCard';
 import MeetingModal from './MeetingModal';
-import { useUser } from '@clerk/nextjs';
-import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useToast } from '@/components/ui/use-toast';
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -21,11 +22,18 @@ const MeetingTypeList = () => {
     link: '',
   });
   const [callDetails, setCallDetails] = useState<Call>();
+  const { toast } = useToast();
 
   const createMeeting = async () => {
     if (!client || !user) return;
 
     try {
+      if (!values.dateTime) {
+        toast({
+          title: 'Please select a date and time',
+        });
+        return;
+      }
       const id = crypto.randomUUID();
       const call = client.call('default', id);
 
@@ -49,8 +57,15 @@ const MeetingTypeList = () => {
       if (!values.description) {
         router.push(`/meeting/${call.id}`);
       }
+
+      toast({
+        title: 'Meeting Created',
+      });
     } catch (error) {
       console.log(error);
+      toast({
+        title: 'Failed to create meeting',
+      });
     }
   };
 
